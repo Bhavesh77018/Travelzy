b5767178037eb9698d9a7a67491b603440b033d3
AD\Bhavesh.aggarwal <Bhaveshaggarwal143@gmail.com>
2026-01-09 15:38:32 +0530

    fix(vercel): correct path to dist folder in api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,3 @@
-const app = require('./dist/index.js').default || require('./dist/index.js');
+const app = require('../dist/index.js').default || require('../dist/index.js');
 
 module.exports = app;
